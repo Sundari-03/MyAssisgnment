101d546815b3b0a0f3bc0208a27501f98ee9397e
Sundari-03 <sundaribavya@gmail.com>
2024-12-07 22:28:20 +0530

    Add files via upload
    day1Assignment

---
+++ Day1/Chrome.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBetatype = false;
+		int releaseYear = 2008;
+		char shortCutkey = 'C';
+		
+		System.out.println("version is " + version);
+		System.out.println(" developer is " + developer);
+		System.out.println("isBetatype is " + isBetatype);
+		System.out.println("releaseYear is " + releaseYear);
+		System.out.println("shortCutkey is " + shortCutkey);
+			
+
+	}
+
+}
---
+++ Day1/FireFox.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f; 
+		String browserName = "fireFox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("browserVersion is " +browserVersion);
+		System.out.println("browserName is "+ browserName);
+		System.out.println("isVisible is " + isVisible);
+		System.out.println("releaseYear is " + releaseYear);
+		System.out.println("browserLogo is " +browserLogo);  
+	}
+
+}
